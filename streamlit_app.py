7ad84d5fd22623e21e8600e3f6c1282c68fcba35
snowshubh <148942649+snowshubh@users.noreply.github.com>
2023-10-25 12:44:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,5 @@ streamlit.text('🥗 kale, Spinac & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
    
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
